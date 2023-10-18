22a94c7108a26c6d84051be5d9f30f8ae6d9ea95
Faelixx <sasha.taylor7@gmail.com>
2023-10-18 04:53:56 -0600

    Added comments.

--- head.js
+++ head.js
@@ -16,6 +16,7 @@ const head = function(array) {
   return array[0];
 }
 
+//Test cases
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([0]), 0);
