df4461369b09d8ce7130506b145796f9e928c138
Faelixx <sasha.taylor7@gmail.com>
2023-10-18 17:08:56 -0600

    Updated checks, fixed console messages.

--- flatten.js
+++ flatten.js
@@ -29,14 +29,15 @@ and flattens it into one single level array.
 // Create a function that receives the array input
 
 const flatten = function(arr1) {
-// Make an empty array to join the arrays
+// Make an empty array to merge the arrays
   let flattened = [];
   // Check items in array for nested arrays
   for (let i = 0; i < arr1.length; i++) {
     if (Array.isArray(arr1[i])) {
-      // Once array within array has been found, loop it maybe?
-      flattened = flattened.concat(flatten(arr1[i]));
+      // Once array within array has been found, begin the array merging.
+      flattened = flattened.concat(arr1[i]);
       
+      // Non array elements pushed into the new array.
     } else {
       flattened.push(arr1[i]);
     }
@@ -45,3 +46,4 @@ const flatten = function(arr1) {
 };
 
 flatten([1, 2, [3, 4], 5]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5]), [1, 2, 3, 4, 5]);
