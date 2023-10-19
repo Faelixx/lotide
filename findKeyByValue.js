234a20075f3a04a3b563122b05928eaca15de4ba
Faelixx <sasha.taylor7@gmail.com>
2023-10-19 11:53:31 -0600

    Updated function, added alternative.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -16,31 +16,51 @@ const assertEqual = function(actual, expected) {
 
 /*
 Write a function that takes in an object for the first argument and a
-key property for the second argument. 
+key property for the second argument.
 The purpose of this object is to return the key of the second argument
 (key property) that is found within the first argument (object).
 */
 
-const findKeyByValue = function (object, property) {
-  /* Create a loop to check each key properties in the object argument
-  for a match.
-  */
-  for (objects in object) {
-    // Condition to check if property argument is a match to a loop iteration
-    if (object[objects] === property) {
-      // Resturn object key
-      return objects;
+const findKeyByValue = function(object, property) {
+  // Create an empty array to store our object keys
+  const objArr = Object.keys(object);
+
+  // Create a loop for our object key array.
+  for (const properties of objArr) {
+    // Condidion to check if property argument is a match to a loop iteration
+    // Note, the loop iteration from the array is used as a key for our object argument in the function
+    if (object[properties] === property) {
+      // Return array iteration
+      return properties;
     }
   }
+
 };
+
+/*
+Alternative version below
+*/
+
+// const findKeyByValue = function (object, property) {
+//   /* Create a loop to check each key properties in the object argument
+//   for a match.
+//   */
+//   for (const objects in object) {
+//     // Condition to check if property argument is a match to a loop iteration
+//     if (object[objects] === property) {
+//       // Return object key
+//       return objects;
+//     }
+//   }
+// };
 
 // Test Cases
 
 const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire"
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70's Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70s Show"), undefined);
