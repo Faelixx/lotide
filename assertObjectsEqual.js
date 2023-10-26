4fe675cfbee29590095db72885229db9190258da
Faelixx <sasha.taylor7@gmail.com>
2023-10-26 12:04:25 -0600

    Further debugging for function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -30,25 +30,25 @@ const eqObjects = function(object1, object2) {
 // Create two arrays for each respective object keys for future comparison.
   const objArr1 = Object.keys(object1);
   const objArr2 = Object.keys(object2);
-  let result = false;
+  let result = true;
 
   // Check that both object key arrays have the same length.
   if (objArr1.length === objArr2.length) {
     // For of loop to ensure that the key arrays match in values.
     for (const objArr1Key of objArr1) {
-      // console.log(Array.isArray(object1[objArr1Key]), Array.isArray(object2[objArr1Key]), eqArrays(object1[objArr1Key], object2[objArr1Key])); // test log
       if (Object.prototype.hasOwnProperty.call(object2, objArr1Key)) {
-        
         if (Array.isArray(object1[objArr1Key]) && Array.isArray(object2[objArr1Key])) {
           result = eqArrays(object1[objArr1Key], object2[objArr1Key]);
           if (result === false) {
             return result;
           }
-        } else if (object1[objArr1Key] === object2[objArr1Key]) {
-          result = true;
-        }
+        } else if (object1[objArr1Key] !== object2[objArr1Key]) {
+          result = false;
+        } 
       }
     } return result;
+  } else {
+    result = false;
   } return result;
 
 
--- eqObjects.js
+++ eqObjects.js
@@ -29,32 +29,32 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const eqObjects = function(object1, object2) {
-// Create two arrays for each respective object keys for future comparison.
-  const objArr1 = Object.keys(object1);
-  const objArr2 = Object.keys(object2);
-  let result = false;
-
-  // Check that both object key arrays have the same length.
-  if (objArr1.length === objArr2.length) {
-    // For of loop to ensure that the key arrays match in values.
-    for (const objArr1Key of objArr1) {
-      // console.log(Array.isArray(object1[objArr1Key]), Array.isArray(object2[objArr1Key]), eqArrays(object1[objArr1Key], object2[objArr1Key])); // test log
-      if (Object.prototype.hasOwnProperty.call(object2, objArr1Key)) {
-        
-        if (Array.isArray(object1[objArr1Key]) && Array.isArray(object2[objArr1Key])) {
-          result = eqArrays(object1[objArr1Key], object2[objArr1Key]);
-          if (result === false) {
-            return result;
-          }
-        } else if (object1[objArr1Key] === object2[objArr1Key]) {
-          result = true;
+  // Create two arrays for each respective object keys for future comparison.
+    const objArr1 = Object.keys(object1);
+    const objArr2 = Object.keys(object2);
+    let result = true;
+  
+    // Check that both object key arrays have the same length.
+    if (objArr1.length === objArr2.length) {
+      // For of loop to ensure that the key arrays match in values.
+      for (const objArr1Key of objArr1) {
+        if (Object.prototype.hasOwnProperty.call(object2, objArr1Key)) {
+          if (Array.isArray(object1[objArr1Key]) && Array.isArray(object2[objArr1Key])) {
+            result = eqArrays(object1[objArr1Key], object2[objArr1Key]);
+            if (result === false) {
+              return result;
+            }
+          } else if (object1[objArr1Key] !== object2[objArr1Key]) {
+            result = false;
+          } 
         }
-      }
+      } return result;
+    } else {
+      result = false;
     } return result;
-  } return result;
-
-
-};
+  
+  
+  };
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
@@ -65,7 +65,7 @@ assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
 console.log();
 
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue", "green"] };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
 console.log();
 const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
