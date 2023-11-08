4cffc83b62910d075e657bb7baf9bc670c91bf0e
Faelixx <sasha.taylor7@gmail.com>
2023-11-08 00:20:09 -0700

    Minor project fix after AI feedback

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,44 +1,44 @@
-const eqArrays = function(arr1, arr2) {
-  // Check if both arrays are the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  // Compare each element in the arrays
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function(arr1, arr2) {
+//   // Check if both arrays are the same length
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   // Compare each element in the arrays
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const eqObjects = function(object1, object2) {
-// Create two arrays for each respective object keys for future comparison.
-  const objArr1 = Object.keys(object1);
-  const objArr2 = Object.keys(object2);
-  let result = true;
+// const eqObjects = function(object1, object2) {
+// // Create two arrays for each respective object keys for future comparison.
+//   const objArr1 = Object.keys(object1);
+//   const objArr2 = Object.keys(object2);
+//   let result = true;
 
-  // Check that both object key arrays have the same length.
-  if (objArr1.length === objArr2.length) {
-    // For of loop to ensure that the key arrays match in values.
-    for (const objArr1Key of objArr1) {
-      if (Object.prototype.hasOwnProperty.call(object2, objArr1Key)) {
-        if (Array.isArray(object1[objArr1Key]) && Array.isArray(object2[objArr1Key])) {
-          result = eqArrays(object1[objArr1Key], object2[objArr1Key]);
-          if (result === false) {
-            return result;
-          }
-        } else if (object1[objArr1Key] !== object2[objArr1Key]) {
-          result = false;
-        } 
-      }
-    } return result;
-  } else {
-    result = false;
-  } return result;
+//   // Check that both object key arrays have the same length.
+//   if (objArr1.length === objArr2.length) {
+//     // For of loop to ensure that the key arrays match in values.
+//     for (const objArr1Key of objArr1) {
+//       if (Object.prototype.hasOwnProperty.call(object2, objArr1Key)) {
+//         if (Array.isArray(object1[objArr1Key]) && Array.isArray(object2[objArr1Key])) {
+//           result = eqArrays(object1[objArr1Key], object2[objArr1Key]);
+//           if (result === false) {
+//             return result;
+//           }
+//         } else if (object1[objArr1Key] !== object2[objArr1Key]) {
+//           result = false;
+//         } 
+//       }
+//     } return result;
+//   } else {
+//     result = false;
+//   } return result;
 
 
-};
+// };
 
 const assertObjectsEqual  = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -50,4 +50,6 @@ const assertObjectsEqual  = function(actual, expected) {
   }
 };
 
-assertObjectsEqual({ a: 1, b: 2, c: 3,}, { a: 1, b: 2, c: 3,});
+module.exports = assertObjectsEqual;
+
+// assertObjectsEqual({ a: 1, b: 2, c: 3,}, { a: 1, b: 2, c: 3,});
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
@@ -7,7 +10,6 @@ const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const head = require('./head');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const middle = require('./middle');
 const tail = require('./tail');
 const takeUntil = require('./takeUntil');
@@ -15,6 +17,9 @@ const without = require('./without');
 
 
 module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
   countLetters: countLetters,
   countOnly: countOnly,
   eqArrays: eqArrays,
@@ -24,7 +29,6 @@ module.exports = {
   flatten: flatten,
   head: head,
   letterPositions: letterPositions,
-  map: map,
   middle: middle,
   tail: tail,
   takeUntil: takeUntil,
