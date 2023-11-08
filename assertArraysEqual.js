9ac05785defc5e0e2afb28987ab391c1ab7859a1
Faelixx <sasha.taylor7@gmail.com>
2023-11-07 20:54:05 -0700

    Dried up code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,7 +3,7 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(arr1, arr2) {
   // Function output into a variable to create a comparison.
   let arrayCheck = eqArrays(arr1, arr2);
-  if (arrayCheck === true) {
+  if (arrayCheck) {
     console.log(`╰(*°▽°*)╯ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`(┬┬﹏┬┬) Assertion Failed: ${arr1} !== ${arr2}`);
