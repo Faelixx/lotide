a66c76db7f8975b8d6bfdc6fca8e7b2ea8c3aa67
Faelixx <sasha.taylor7@gmail.com>
2023-11-07 20:43:37 -0700

    Added more test cases

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -4,3 +4,6 @@ const assertEqual = require('../assertEqual');
 // Test Case
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+assertEqual(eqArrays([2, 2, 3], [1, 2, 4]), false);
+assertEqual(eqArrays([2, 2, 3, 5, 7], [2, 2, 3, 5, 7]), true);
